b737d4bf54151dd873837d9d3a68b7014d6b8985
asoru <wan8441@naver.com>
2024-10-02 01:04:18 +0900

    skill.cs

--- Assets/Script/ElementManager.cs
+++ Assets/Script/ElementManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ElementManager : MonoBehaviour
 {
+    private SkillManager skillManager;
     public Boolean skill_Q = true;
     public Boolean skill_E = true;
 
@@ -13,18 +14,22 @@ public class ElementManager : MonoBehaviour
 
     // Pyro, Hydro, Anemo, Geo
     public List<String> Element = new List<String> { "Pyro", "Hydro", "Anemo", "Geo" };
+    public List<float> QSkillDelay = new List<float> { 2f, 3f, 2f, 3f };
+    public List<float> ESkillDelay = new List<float> { 7f, 8f, 8f, 7f };
     private int currentElement = 0;
+
+    void Start() { skillManager = GetComponent<SkillManager>(); }
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Q))      // Q스킬사용
         {
-            Use1Skill();
+            UseQSkill();
         }
 
         if (Input.GetKeyUp(KeyCode.E))      // E스킬사용
         {
-            Use2Skill();
+            UseESkill();
         }
 
         if (Input.GetKeyUp(KeyCode.Tab))    // 원소바꾸기
@@ -33,44 +38,58 @@ public class ElementManager : MonoBehaviour
         }
 
     }
+
     void ChangeElement()
     {
         currentElement = (currentElement + 1) % Element.Count;
         Debug.Log("현재 원소: " + Element[currentElement]);
     }
 
-    private void Use1Skill()
+    private void UseQSkill()
     {
+        float delay = QSkillDelay[currentElement];
+
         if (skill_Q)
         {
-            StartCoroutine(SkillQDelayCoroutine(2f));
+            Debug.Log("Q스킬 사용 중, currentElement: " + currentElement);
+            skillManager.QSkill(currentElement);
+            StartCoroutine(QSkillDelayCoroutine(delay));
+        }
+        else
+        {
+            Debug.Log("Q스킬 사용 불가, 쿨타임 중");
         }
     }
-    private IEnumerator SkillQDelayCoroutine(float delay)
+    private IEnumerator QSkillDelayCoroutine(float delay)
     {
         skill_Q = false;
-        Debug.Log("스킬1 사용!!");
+        Debug.Log("Q스킬 쿨타임 중");
         yield return new WaitForSeconds(delay);
-        Debug.Log("스킬1 사용가능");
+        Debug.Log("Q스킬 사용가능");
         skill_Q = true;
     }
 
-    private void Use2Skill()
+    private void UseESkill()
     {
+        float delay = ESkillDelay[currentElement];
         if (skill_E)
         {
+            skillManager.ESkill(currentElement);
             StartCoroutine(SkillEDelayCoroutine(7f));
+        }
+        else
+        {
+            Debug.Log("E스킬 사용 불가, 쿨타임 중");
         }
     }
     private IEnumerator SkillEDelayCoroutine(float delay)
     {
         skill_E = false;
-        Debug.Log("스킬2 사용!!");
+        Debug.Log("E스킬 쿨타임 중!!");
         yield return new WaitForSeconds(delay);
-        Debug.Log("스킬2 사용가능");
+        Debug.Log("E스킬 사용가능");
         skill_E = true;
     }
 
-    
 
 }
--- Assets/Script/SkillManager.cs
+++ Assets/Script/SkillManager.cs
@@ -4,43 +4,74 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    public void QSkill(int Element)
+    {
+        switch (Element)
+        {
+            case 0:
+                PyroQSkill(); break;
+            case 1:
+                HydroQSkill(); break;
+            case 2:
+                AnemoQSkill(); break;
+            case 3:
+                GeoQSkill(); break;
+        }
+    }
+
+    public void ESkill(int Element)
+    {
+        switch (Element)
+        {
+            case 0:
+                PyroESkill(); break;
+            case 1:
+                HydroESkill(); break;
+            case 2:
+                AnemoESkill(); break;
+            case 3:
+                GeoESkill(); break;
+        }
+
+    }
+
     public void PyroQSkill()
     {
-
+        Debug.Log("Pyro Q Skill Atcivity");
     }
 
     public void PyroESkill()
     {
-
+        Debug.Log("Pyro E Skill Atcivity");
     }
 
     public void HydroQSkill()
     {
-
+        Debug.Log("Hydro Q Skill Atcivity");
     }
 
     public void HydroESkill()
     {
-
+        Debug.Log("Hydro E Skill Atcivity");
     }
 
     public void AnemoQSkill()
     {
-
+        Debug.Log("Anemo Q Skill Atcivity");
     }
 
     public void AnemoESkill()
     {
-
+        Debug.Log("Anemo E Skill Atcivity");
     }
 
     public void GeoQSkill()
     {
-
+        Debug.Log("Geo Q Skill Atcivity");
     }
 
     public void GeoESkill()
     {
-
+        Debug.Log("Geo E Skill Atcivity");
     }
 }
