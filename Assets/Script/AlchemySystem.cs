29d45c928092c6a531929ab1da4c71b81bce38c2
YunHyunSup <122049787+YunHyunSup@users.noreply.github.com>
2024-11-25 05:05:45 +0900

    빌드 오류 수정+아이콘

--- Assets/Script/AlchemySystem.cs
+++ Assets/Script/AlchemySystem.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using Unity.PlasticSCM.Editor.WebApi;
 
 public class AlchemySystem : MonoBehaviour
 {
--- Assets/Script/ItemInformation.cs
+++ Assets/Script/ItemInformation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor.PackageManager.UI;
 
 public class ItemInformation : MonoBehaviour
 {
